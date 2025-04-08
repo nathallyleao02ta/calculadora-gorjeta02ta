b6a4c11dc3a2e623a09498208a355d8c9a146d97
nathallybarbosasaoleao <nathallybarbosasaoleao@gmail.com>
2025-04-08 13:29:01 +0000

    função para receber o valor customizado da gorjeta

--- main.js
+++ main.js
@@ -15,12 +15,30 @@ function receiveTipPercentageValue(value) {
     if (buttonSelected!== null){
         buttonSelected.classList.remove("button-selected")
 }
+   let customTipImput = document.querySelector("#custom-tip").ValueAsNumber
+    
+   console.log(customTipImput)
+   
+   if (customTipImput!==""){
+    customTipImput.value =  ""
+
+   }
+
+
     tipPercentage = value / 100
     console.log(tipPercentage)
 
     buttonSelected = document.querySelector('#button-${value}')
     buttonSelected.classList.add("buttonSelected")
+}
 
-    
+function receiveCustonTipPercentageValue(){
+  if (buttonSelected!== null){
+  buttonSelected.classList.remove("button-selected")
+  buttonSelected = null 
+ }
+
+ tipPercentage = document.querySelector()
+
 }
    
