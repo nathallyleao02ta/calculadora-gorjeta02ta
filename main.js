1b67832343cd8a836ad573df3609674cc3c9e0ed
nathallybarbosasaoleao <nathallybarbosasaoleao@gmail.com>
2025-04-08 13:53:01 +0000

    função para calcular a gorjeta e o total de pessoa finalizada

--- main.js
+++ main.js
@@ -5,20 +5,23 @@ let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill")
+    calculate()
 }
 
 function receiveNumberOfPeopleValue(){
     numberOfPeople = documen.querySelector("#people").ValueAsNumber
+    calculate()
 }
 
 function receiveTipPercentageValue(value) {
     if (buttonSelected!== null){
         buttonSelected.classList.remove("button-selected")
+        calculate()
 }
    let customTipImput = document.querySelector("#custom-tip").ValueAsNumber
     
    console.log(customTipImput)
-   
+
    if (customTipImput!==""){
     customTipImput.value =  ""
 
@@ -38,7 +41,23 @@ function receiveCustonTipPercentageValue(){
   buttonSelected = null 
  }
 
- tipPercentage = document.querySelector()
+ tipPercentage = document.querySelector("#custom-Tip").ValueAsNumber / 100
+ calculate()
 
 }
-   
+
+function calculate(){
+  if(bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0)
+    let amoutStrong = document.querySelector(".amout strong")
+    let tipAmoutPerson = (bill * tipPercentage) / numberOfPeople
+    amoutStrong.innerText = '$${tipAmoutPerson.toFixed(2)}'
+
+    let totalStrong = document.querySelector(".total strong")
+    let totalPerson = (bill / numberOfPeople) + tipAmoutPerson
+    totalStrong.innerText = '$${totalPerson.toFixed(2)}'
+
+
+   } else {
+    console.log("preencha tudo")
+   }
+ }
