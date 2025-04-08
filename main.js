681d46871282876ca301402a18b2e4fd30c460ca
nathallybarbosasaoleao <nathallybarbosasaoleao@gmail.com>
2025-04-08 13:11:55 +0000

    recebendo a porcentagem da gorjeta ao clicar em qualquer um dos botões

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 let bill = 0
 let tipPercentage = 0 
-let numberOfPeople = 0
+let numberOfPeople = 
+let buttonSelected = null
 
 function receiveBillValue() {
-    bill = document.querySelector("#bill").
+    bill = document.querySelector("#bill")
 }
 
 function receiveNumberOfPeopleValue(){
     numberOfPeople = documen.querySelector("#people").ValueAsNumber
+}
+
+function receiveTipPercentageValue(value) {
+    if (buttonSelected!== null){
+        buttonSelected.classList.remove("button-selected")
+}
+    tipPercentage = value / 100
+    console.log(tipPercentage)
+
+    buttonSelected = document.querySelector('#button-${value}')
+    buttonSelected.classList.add("buttonSelected")
+
+    
 }
    
