7a4708ab753e5c2121424a35ff875330fcf81478
nathallybarbosasaoleao <nathallybarbosasaoleao@gmail.com>
2025-04-08 01:00:21 +0000

    função para receber o valor da conta(bill) e função para receber o número de pessoas

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+let bill = 0
+let tipPercentage = 0 
+let numberOfPeople = 0
 
+function receiveBillValue() {
+    bill = document.querySelector("#bill").
+}
+
+function receiveNumberOfPeopleValue(){
+    numberOfPeople = documen.querySelector("#people").ValueAsNumber
+}
+   
